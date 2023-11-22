512813637869550f1f8751cbbebe11a559ae39a0
alex <alex.schlessinger@gmail.com>
2023-11-21 20:53:28 -0800

    cleanup

--- src/gfx.zig
+++ src/gfx.zig
@@ -6,8 +6,8 @@ const rnd = @import("random.zig");
 
 const ogwindowwidth: i32 = 640;
 const ogwindowheight: i32 = 760;
-var windowwidth: i32 = 640;
-var windowheight: i32 = 760;
+var windowwidth: i32 = ogwindowwidth;
+var windowheight: i32 = ogwindowheight;
 var gridoffsetx: i32 = 150;
 var gridoffsety: i32 = 50;
 var cellsize: i32 = 35;
@@ -21,7 +21,9 @@ const images: [4][*:0]const u8 = .{
     "resources/texture/bokefall.png",
 };
 
+var texture = ray.RenderTexture2D{};
 var spacefont = ray.Font{};
+
 // shader stuff
 var bgshader: ray.Shader = undefined;
 var bgtexture: ray.Texture2D = undefined;
@@ -40,33 +42,34 @@ var ampY: f32 = 2.0;
 var speedX: f32 = 0.25;
 var speedY: f32 = 0.25;
 
-var texture = ray.RenderTexture2D{};
 pub fn frame() void {
+    // resize
     updatescale();
+    // shader uniforms
     preshade();
-
     ray.BeginDrawing();
     // draw to texture first
     ray.BeginTextureMode(texture);
-
+    // background and shader
     background();
+    // player piece and ghost
     player();
+    // grid of cemented cells
     grid();
+    // animation for line clears
     lineclears();
+    // ux
     ui();
-
     ray.EndTextureMode();
     // scale texture to window size
     var src = ray.Rectangle{ .x = 0, .y = 0, .width = ogwindowwidth, .height = -ogwindowheight };
     var tgt = ray.Rectangle{ .x = 0, .y = 0, .width = @floatFromInt(windowwidth), .height = @floatFromInt(windowheight) };
     ray.DrawTexturePro(texture.texture, src, tgt, ray.Vector2{ .x = 0, .y = 0 }, 0, ray.WHITE);
-
     ray.EndDrawing();
 }
 
 pub fn init() !void {
     std.debug.print("init gfx\n", .{});
-    // ignore me
 
     // window init
     ray.SetConfigFlags(ray.FLAG_MSAA_4X_HINT | ray.FLAG_WINDOW_RESIZABLE);
@@ -95,7 +98,7 @@ pub fn deinit() void {
     std.debug.print("deinit gfx\n", .{});
     ray.UnloadShader(bgshader);
     ray.UnloadTexture(bgtexture);
-    ray.UnloadTexture(spacefont.texture);
+    ray.UnloadTexture(texture.texture);
     ray.UnloadFont(spacefont);
 }
 
@@ -157,6 +160,7 @@ fn preshade() void {
     ray.SetShaderValue(bgshader, ampYLoc, &ampY, ray.SHADER_UNIFORM_FLOAT);
     ray.SetShaderValue(bgshader, speedXLoc, &speedX, ray.SHADER_UNIFORM_FLOAT);
     ray.SetShaderValue(bgshader, speedYLoc, &speedY, ray.SHADER_UNIFORM_FLOAT);
+
     var size: [2]f32 = undefined;
     size[0] = @as(f32, @floatFromInt(ray.GetScreenWidth()));
     size[1] = @as(f32, @floatFromInt(ray.GetScreenHeight()));
@@ -166,18 +170,19 @@ fn preshade() void {
 fn background() void {
     ray.ClearBackground(ray.BLACK);
     ray.BeginShaderMode(bgshader);
+
     var src = ray.Rectangle{
         .x = 0,
         .y = 0,
-        .width = @as(f32, @floatFromInt(bgtexture.width)),
-        .height = @as(f32, @floatFromInt(bgtexture.height)),
+        .width = @floatFromInt(bgtexture.width),
+        .height = @floatFromInt(bgtexture.height),
     };
 
     var tgt = ray.Rectangle{
         .x = 0,
         .y = 0,
-        .width = @as(f32, @floatFromInt(ogwindowwidth)),
-        .height = @as(f32, @floatFromInt(ogwindowheight)),
+        .width = @floatFromInt(ogwindowwidth),
+        .height = @floatFromInt(ogwindowheight),
     };
 
     ray.DrawTexturePro(bgtexture, src, tgt, ray.Vector2{ .x = 0, .y = 0 }, 0, ray.WHITE);
@@ -192,8 +197,8 @@ fn lineclears() void {
                 for (0..10) |colIndex| {
                     var x = @as(i32, @intCast(colIndex)) * cellsize;
                     var y = @as(i32, @intCast(rowIndex)) * cellsize;
-                    var e = @as(f32, @floatFromInt(elapsed_time));
-                    var d = @as(f32, @floatFromInt(game.state.lineclearer.duration));
+                    var e: f32 = @floatFromInt(elapsed_time);
+                    var d: f32 = @floatFromInt(game.state.lineclearer.duration);
                     // clamp between 0 and 255
                     const computed = e / d * 255.0;
                     const clamped = std.math.clamp(computed, 0.0, 255.0);
@@ -201,7 +206,6 @@ fn lineclears() void {
 
                     // find underlying cell
                     var cell = game.state.cells[rowIndex][colIndex];
-
                     const color = .{ cell[0], cell[1], cell[2], 255 - ratio };
                     roundedfillbox(x, y, color);
                 }
@@ -221,16 +225,16 @@ fn player() void {
     if (game.state.piece) |p| {
         var drawX = game.state.piecex * cellsize;
         var drawY = game.state.piecey * cellsize;
-        var fdrawx: f32 = @as(f32, @floatFromInt(drawX));
-        var fdrawy: f32 = @as(f32, @floatFromInt(drawY));
+        var fdrawx: f32 = @floatFromInt(drawX);
+        var fdrawy: f32 = @floatFromInt(drawY);
 
         const elapsed_time = std.time.milliTimestamp() - game.state.pieceslider.start_time;
         // animate the piece if the slider is active
         if (game.state.pieceslider.active) {
-            var duration = @as(f32, @floatFromInt(game.state.pieceslider.duration));
+            var duration: f32 = @floatFromInt(game.state.pieceslider.duration);
             const ratio: f32 = std.math.clamp(@as(f32, @floatFromInt(elapsed_time)) / duration, 0.0, 1.0);
-            var targetx = @as(f32, @floatFromInt(game.state.pieceslider.targetx * cellsize));
-            var targety = @as(f32, @floatFromInt(game.state.pieceslider.targety * cellsize));
+            var targetx: f32 = @floatFromInt(game.state.pieceslider.targetx * cellsize);
+            var targety: f32 = @floatFromInt(game.state.pieceslider.targety * cellsize);
             // lerp between the current position and the target position
             fdrawx = std.math.lerp(fdrawx, targetx, ratio);
             fdrawy = std.math.lerp(fdrawy, targety, ratio);
@@ -244,8 +248,8 @@ fn player() void {
             }
         }
 
-        var xdx = @as(i32, @intFromFloat(fdrawx));
-        var ydx = @as(i32, @intFromFloat(fdrawy));
+        var xdx: i32 = @intFromFloat(fdrawx);
+        var ydx: i32 = @intFromFloat(fdrawy);
 
         // draw the piece at the interpolated position
         piece(xdx, ydx, p.shape[game.state.piecer], p.color);
@@ -261,7 +265,9 @@ fn piece(x: i32, y: i32, shape: [4][4]bool, color: [4]u8) void {
     for (shape, 0..) |row, i| {
         for (row, 0..) |cell, j| {
             if (cell) {
-                roundedfillbox(x + @as(i32, @intCast(i)) * cellsize, y + @as(i32, @intCast(j)) * cellsize, color);
+                var xs: i32 = @as(i32, @intCast(i)) * cellsize;
+                var ys: i32 = @as(i32, @intCast(j)) * cellsize;
+                roundedfillbox(x + xs, y + ys, color);
             }
         }
     }
@@ -270,10 +276,10 @@ fn piece(x: i32, y: i32, shape: [4][4]bool, color: [4]u8) void {
 // draw a box
 fn box(x: i32, y: i32, color: [4]u8) void {
     ray.DrawRectangleLinesEx(ray.Rectangle{
-        .x = @as(f32, @floatFromInt(gridoffsetx + x)),
-        .y = @as(f32, @floatFromInt(gridoffsety + y)),
-        .width = @as(f32, @floatFromInt(getcellwidth())),
-        .height = @as(f32, @floatFromInt(getcellwidth())),
+        .x = @floatFromInt(gridoffsetx + x),
+        .y = @floatFromInt(gridoffsety + y),
+        .width = @floatFromInt(getcellwidth()),
+        .height = @floatFromInt(getcellwidth()),
     }, 2, ray.Color{
         .r = color[0],
         .g = color[1],
@@ -295,10 +301,10 @@ fn fillbox(x: i32, y: i32, color: [4]u8) void {
 // draw a rounded box
 fn roundedfillbox(x: i32, y: i32, color: [4]u8) void {
     ray.DrawRectangleRounded(ray.Rectangle{
-        .x = @as(f32, @floatFromInt(gridoffsetx + x)),
-        .y = @as(f32, @floatFromInt(gridoffsety + y)),
-        .width = @as(f32, @floatFromInt(getcellwidth())),
-        .height = @as(f32, @floatFromInt(getcellwidth())),
+        .x = @floatFromInt(gridoffsetx + x),
+        .y = @floatFromInt(gridoffsety + y),
+        .width = @floatFromInt(getcellwidth()),
+        .height = @floatFromInt(getcellwidth()),
     }, 0.5, 50, ray.Color{
         .r = color[0],
         .g = color[1],
@@ -329,7 +335,6 @@ fn getcellwidth() i32 {
 var textbuf: [1000]u8 = undefined;
 fn ui() void {
     ray.SetTextLineSpacing(22);
-
     var bordercolor = ray.Color{
         .r = 0,
         .g = 0,
@@ -343,11 +348,11 @@ fn ui() void {
     ray.DrawLine(ogwindowwidth - 135, 0, ogwindowwidth - 135, ogwindowheight, ray.RED);
     if (std.fmt.bufPrintZ(&textbuf, "score\n{}\nlines\n{}\nlevel\n{}", .{ game.state.score, game.state.lines, game.state.level })) |score| {
         var color = ray.GREEN;
-        var size: f32 = 22 * scalefactor;
+        var size: f32 = 22;
         if (game.state.lineclearer.active) {
-            scramblefx(score);
+            scramblefx(score, 10);
             color = ray.RED;
-            size = 30 * scalefactor;
+            size = 30;
         }
         // var scoreheight = ray.MeasureTextEx(sys.spacefont, score, size, 3).y;
         // var scorey = @as(f32, @floatFromInt(windowheight)) - scoreheight * 2;
@@ -380,8 +385,13 @@ fn ui() void {
             .a = 210,
         });
 
-        ray.DrawTextEx(spacefont, "PAUSED", ray.Vector2{ .x = 200, .y = 300 }, 60, 3, ray.ORANGE);
-        ray.DrawText("press p to unpause", 210, 350, 20, ray.RED);
+        if (std.fmt.bufPrintZ(&textbuf, "PAUSED", .{})) |paused| {
+            scramblefx(paused, 10);
+            ray.DrawTextEx(spacefont, paused, ray.Vector2{ .x = 210, .y = 300 }, 60, 3, ray.ORANGE);
+            ray.DrawText("press p to unpause", 220, 350, 20, ray.RED);
+        } else |err| {
+            std.debug.print("error printing paused: {}\n", .{err});
+        }
     }
 
     if (game.state.gameover) {
@@ -392,21 +402,26 @@ fn ui() void {
             .a = 200,
         });
 
-        ray.DrawTextEx(spacefont, "GAME OVER", ray.Vector2{ .x = 145, .y = 290 }, 60, 3, ray.RED);
-        ray.DrawText("r to restart", 255, 350, 20, ray.WHITE);
-        ray.DrawText("esc to exit", 255, 375, 20, ray.WHITE);
+        if (std.fmt.bufPrintZ(&textbuf, "GAME OVER", .{})) |over| {
+            scramblefx(over, 1);
+            ray.DrawTextEx(spacefont, over, ray.Vector2{ .x = 145, .y = 290 }, 60, 3, ray.RED);
+            ray.DrawText("r to restart", 255, 350, 20, ray.WHITE);
+            ray.DrawText("esc to exit", 255, 375, 20, ray.WHITE);
+        } else |err| {
+            std.debug.print("error printing game over: {}\n", .{err});
+        }
     }
 }
 
 const scrambles = "!@#$%^&*+-=<>?/\\|~`";
-fn scramblefx(s: []u8) void {
+fn scramblefx(s: []u8, intensity: i32) void {
     for (s) |*c| {
         var n = scrambles[rnd.ng.random().intRangeAtMost(u32, 0, scrambles.len)];
-        if (c.* == '\n') {
+        if (c.* == '\n' or c.* == ' ') {
             continue;
         }
 
-        if (rnd.ng.random().intRangeAtMost(u32, 0, 100) > 90) {
+        if (rnd.ng.random().intRangeAtMost(u32, 0, 100) > 100 - intensity) {
             c.* = n;
         }
     }
