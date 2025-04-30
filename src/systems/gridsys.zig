f40f16ba74515625825f926c47048c95cbec9ddf
alex schlessinger <alex.schlessinger@gmail.com>
2025-04-30 11:38:43 -0700

    rm unused

--- src/systems/gridsys.zig
+++ src/systems/gridsys.zig
@@ -16,8 +16,6 @@ pub fn gridRenderSystem() void {
 fn renderStaticBlocks() void {
     const world = ecs.getWorld();
 
-    // Query for entities with BlockTag, GridPos, Sprite, and Position components
-    // but WITHOUT Flash component (meaning they've completed their flash animation)
     var view = world.view(.{ components.BlockTag, components.GridPos, components.Sprite, components.Position }, .{components.Flash}); // Exclude entities that still have Flash
 
     var it = view.entityIterator();
@@ -29,58 +27,7 @@ fn renderStaticBlocks() void {
         // Draw the static block
         const draw_x = @as(i32, @intFromFloat(pos.x));
         const draw_y = @as(i32, @intFromFloat(pos.y));
+
         gfx.drawbox(draw_x, draw_y, sprite.rgba, sprite.size);
     }
 }
-
-// fn renderFromGridData() void {
-//     const grid = &game.state.grid;
-//     // Get window parameters from the game globals
-//     const window = gfx.window;
-//     const gridoffsetx = window.gridoffsetx;
-//     const gridoffsety = window.gridoffsety;
-//     const cellsize = window.cellsize;
-//     const cellpadding = window.cellpadding;
-
-//     // Draw grid cells
-//     for (0..Grid.HEIGHT) |y| {
-//         for (0..Grid.WIDTH) |x| {
-//             if (grid.data[y][x]) |cell_data| {
-//                 const drawX = @as(i32, @intCast(x)) * cellsize;
-//                 const drawY = @as(i32, @intCast(y)) * cellsize;
-
-//                 // Calculate scaled dimensions
-//                 const cellsize_scaled = @as(f32, @floatFromInt(cellsize));
-//                 const padding_scaled = @as(f32, @floatFromInt(cellpadding));
-//                 const width_scaled = cellsize_scaled - 2 * padding_scaled;
-
-//                 // Calculate center of cell in screen coordinates
-//                 const center_x = @as(f32, @floatFromInt(gridoffsetx + drawX)) +
-//                     @as(f32, @floatFromInt(cellsize)) / 2.0;
-//                 const center_y = @as(f32, @floatFromInt(gridoffsety + drawY)) +
-//                     @as(f32, @floatFromInt(cellsize)) / 2.0;
-
-//                 // Calculate top-left drawing position
-//                 const rect_x = center_x - width_scaled / 2.0;
-//                 const rect_y = center_y - width_scaled / 2.0;
-
-//                 // Draw rounded rectangle
-//                 // Get color from CellData (which packs it as a u32)
-//                 const rgba = cell_data.toRgba();
-//                 ray.DrawRectangleRounded(ray.Rectangle{
-//                     .x = rect_x,
-//                     .y = rect_y,
-//                     .width = width_scaled,
-//                     .height = width_scaled,
-//                 }, 0.4, // Roundness
-//                     20, // Segments
-//                     ray.Color{
-//                         .r = rgba[0],
-//                         .g = rgba[1],
-//                         .b = rgba[2],
-//                         .a = rgba[3],
-//                     });
-//             }
-//         }
-//     }
-// }
